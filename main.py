b393a99648df7424daae3c34d5a0baa3f07d5e04
Anthony T. Williams <bigant1972@gmail.com>
2026-02-23 09:19:25 -0500

    Correct issues with files

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 from app import app, init_db
 from config.settings import Config
+from utils.notifications import init_mail
+
+# Always runs whether started by Gunicorn or directly
+init_db()
+init_mail(app)
+
+# Load mail settings from Config into Flask app config
+app.config['MAIL_SERVER']   = 'smtp.gmail.com'
+app.config['MAIL_PORT']     = 587
+app.config['MAIL_USE_TLS']  = True
+app.config['MAIL_USERNAME'] = Config.MAIL_USERNAME
+app.config['MAIL_PASSWORD'] = Config.MAIL_PASSWORD
+app.config['MAIL_FROM_NAME'] = Config.MAIL_FROM_NAME
 
 if __name__ == '__main__':
-    init_db()
     app.run(host='0.0.0.0', port=Config.PORT, debug=Config.DEBUG)
-else:
-    init_db()
